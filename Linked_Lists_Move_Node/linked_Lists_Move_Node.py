33f1192be38c16e3c02085b5600f415c9d5fa031
Ivan Zarytskyi <ivan.zarytskyi1991@gmail.com>
2025-03-26 10:29:42 +0200

    Linked Lists - Move Node

---
+++ Linked_Lists_Move_Node/linked_Lists_Move_Node.py
@@ -0,0 +1,18 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+
+def move_node(source, dest):
+    if not source:
+        raise TypeError('Source cannot be None')
+    temp = source.next
+    source.next = dest
+
+    return Context(temp, source)
